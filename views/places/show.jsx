0503f768b9bb855103c882365abd2ce10a90125d
Jeffery Grigsby <jeffery.grigsby@yahoo.com>
2024-06-17 22:20:38 -0700

    Delete functionality implemented

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,14 +1,39 @@
 const React = require('react')
 const Def = require('./default')
 
-function show () {
+function show(data) {
     return (
         <Def>
-          <main>
-            <h1>Show Page</h1>
-          </main>
+            <main>
+                <h1>{data.place.name}</h1>
+                <div>
+                    <img src={data.place.pic} alt={data.place.name} />
+                    <p className="text-center">
+                        Located in {data.place.city}, {data.place.state}
+                    </p>
+                </div>
+                <div>
+                    <h2>Rating</h2>
+                    <p>Currently unrated</p>
+                </div>
+                <div>
+                    <h2>Comments</h2>
+                    <p>No comments yet!</p>
+                </div>
+                <a href={`/places/${data.id}/edit` } className="btn btn-warning">
+                    Edit
+                    </a>
+                    <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+                        <button type="submit" className="btn btn-danger">
+                            Delete
+                        </button>
+                    </form>
+                <a href="/places">
+                    <button className="btn-primary">Back to Places</button>
+                </a>
+            </main>
         </Def>
-    )
+    );
 }
 
 module.exports = show
